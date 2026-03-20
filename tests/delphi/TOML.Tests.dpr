f5a9b3c7b2600886e5f22fab330bf8af30afbdb5
BastianTeufel-GTR <b.teufel@gtr.de>
2026-03-20 12:38:42 +0100

    Add DUnitX test project for Delphi 12 nested key compliance
    12 tests covering parser dotted keys, serializer path tracking,
    and round-trip compliance. All tests pass on Win64.

---
+++ tests/delphi/TOML.Tests.dpr
@@ -0,0 +1,59 @@
+program TOML.Tests;
+
+{$IFNDEF TESTINSIGHT}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+{$STRONGLINKTYPES ON}
+
+uses
+  System.SysUtils,
+  {$IFDEF TESTINSIGHT}
+  TestInsight.DUnitX,
+  {$ENDIF}
+  DUnitX.TestFramework,
+  DUnitX.Loggers.Console,
+  DUnitX.Loggers.Xml.NUnit,
+  TOML in '..\..\src\TOML.pas',
+  TOML.Types in '..\..\src\TOML.Types.pas',
+  TOML.Parser in '..\..\src\TOML.Parser.pas',
+  TOML.Serializer in '..\..\src\TOML.Serializer.pas',
+  TOML.Tests.NestedKeys in 'TOML.Tests.NestedKeys.pas';
+
+var
+  Runner: ITestRunner;
+  Results: IRunResults;
+  Logger: ITestLogger;
+  NUnitLogger: ITestLogger;
+begin
+  try
+    TDUnitX.CheckCommandLine;
+    Runner := TDUnitX.CreateRunner;
+
+    Logger := TDUnitXConsoleLogger.Create(True);
+    Runner.AddLogger(Logger);
+
+    NUnitLogger := TDUnitXXMLNUnitFileLogger.Create(TDUnitX.Options.XMLOutputFile);
+    Runner.AddLogger(NUnitLogger);
+    Runner.FailsOnNoAsserts := False;
+
+    Results := Runner.Execute;
+    if not Results.AllPassed then
+      System.ExitCode := 1
+    else
+      System.ExitCode := 0;
+
+    {$IFNDEF CI}
+    if TDUnitX.Options.ExitBehavior = TDUnitXExitBehavior.Pause then
+    begin
+      System.Write('Done.. press <Enter> key to quit.');
+      System.Readln;
+    end;
+    {$ENDIF}
+  except
+    on E: Exception do
+    begin
+      System.Writeln(E.ClassName, ': ', E.Message);
+      System.ExitCode := 1;
+    end;
+  end;
+end.
